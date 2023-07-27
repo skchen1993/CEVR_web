18bee307ecc92ce960b30fdc62fd53df6b9060fb
skchen1993 <chensykaics.10@nycu.edu.tw>
2023-07-27 21:15:01 +0800

    Result

--- js/result.js
+++ js/result.js
@@ -1,21 +1,4 @@
-<script>
-const container = document.querySelector('.image-container_result');
-const img1 = document.getElementById('img_result1');
-const img2 = document.getElementById('img_result2');
-
-container.addEventListener('mousemove', (e) => {
-  const { left, top, width, height } = container.getBoundingClientRect();
-  const mouseX = e.clientX - left;
-  const mouseY = e.clientY - top;
-
-  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-    // Mouse is inside the image region
-    img1.style.opacity = '0';
-    img2.style.opacity = '1';
-  } else {
-    // Mouse is outside the image region
-    img1.style.opacity = '1';
-    img2.style.opacity = '0';
-  }
-});
-</script>
+function hover(img)
+{
+ img.src = "img/Result_Website_HDR.png"
+}
